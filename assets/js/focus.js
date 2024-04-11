ed85996e780e41c5642113ced7d110e04b195ccb
Sebastian <123086236+16BitMoose@users.noreply.github.com>
2024-04-11 09:46:32 +0200

    Comments for JS

--- assets/js/focus.js
+++ assets/js/focus.js
@@ -3,9 +3,9 @@ const usernameSVG = document.getElementById("usernameSVG");
 const password = document.getElementById("passwordWrapper");
 const passwordSVG = document.getElementById("passwordSVG");
 const passwordConfirm = document.getElementById("passwordConfirmWrapper");
-const passwordConfirmSVG = document.getElementById("passwordConfirmSVG");
+const passwordConfirmSVG = document.getElementById("passwordConfirmSVG"); //Really bad method, but don't know if there is any better way to get all of these elements.
 
-function gainFocus(num){
+function gainFocus(num){ //Switch case would also work
     if(num == 1){
         username.style.borderBottomColor = "#3b51dd";
         usernameSVG.style.stroke = "#3b51dd";
@@ -20,7 +20,7 @@ function gainFocus(num){
     }
 }
 
-function loseFocus(num){
+function loseFocus(num){ //Switch would also work here
     if(num == 1){
         username.style.borderBottomColor = "#f0f0f4a4";
         usernameSVG.style.stroke = "#f0f0f4a4";
